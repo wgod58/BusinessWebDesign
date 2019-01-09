7d65fc0bdcf8382e77539aac31be4beba6cec09d
wgod58 <wgod58@gmail.com>
2019-01-10 00:28:30 +0800

    README.md info vue components to seperate js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BusinessWebDesign
+#Seperated Vue Components to each js
+##Devopled
+execute vuestartup.bat
---
+++ vuestartup.bat
@@ -0,0 +1,8 @@
+:: D:
+:: cd D:\apache-tomcat-9.0.0\webapps\PortalServer
+
+:: call
+
+:: start 可以分開執行
+start .\vue2ts.bat
+start .\ts2js.bat
